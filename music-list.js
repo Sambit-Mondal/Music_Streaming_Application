5d7452190dceb120f9f30f45259006db92ec68c9
Sambit Mondal <sambitmondal2005@gmail.com>
2023-12-30 01:06:41 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,238 @@
+let allMusic = [
+
+    {
+        name: "Harley Bird - Home",
+        artist: "Jordan Scho",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Ikson Anywhere – Ikson",
+        artist: "Audio Library",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Beauz & Jvna - Crazy",
+        artist: "Beauz & Jvna",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Hardwind - Want Me",
+        artist: "Mike Archangelo",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Jim - Sun Goes Down",
+        artist: "Jim Yosef x Roy",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Lost Sky - Vision NCS",
+        artist: "NCS Release",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "On My Way",
+        artist: "Alan Walker",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Faded",
+        artist: "Alan Walker",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "On and On",
+        artist: "Cartoon",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Warriyo - Mortals",
+        artist: "Elport Remix",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Ertugrul Gazi",
+        artist: "Ertugrul",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "Electronic Music",
+        artist: "Electro",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Agar Tum Saath Ho",
+        artist: "Alka Yagnik, Arijit Singh",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Suna Hain",
+        artist: "Jeet Ganguly, Jubin Nautiyal",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "Dilbar",
+        artist: "Neha Kakkar, Dhivani Bhanusali",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "Duniya",
+        artist: "Akhil, Dhivani Bhanusali",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "Lagdi Lahore Di",
+        artist: "Guru Randhawa, Tulsi Kumar",
+        img: "music-17",
+        src: "music-17"
+    },
+    {
+        name: "Putt Jatt Da",
+        artist: "Diljit Dosanjh, Kaater",
+        img: "music-18",
+        src: "music-18"
+    },
+    {
+        name: "Baarishein",
+        artist: "Atif Aslam",
+        img: "music-19",
+        src: "music-19"
+    },
+    {
+        name: "Vaaste",
+        artist: "Dhivani Bhanusali, Nikhil D Souza",
+        img: "music-20",
+        src: "music-20"
+    },
+    {
+        name: "Lut Gaye",
+        artist: "Jubin Nautiyal",
+        img: "music-21",
+        src: "music-21"
+    },
+    {
+        name: "Meri Zindagi Hai Tu",
+        artist: "Jubin Nautiyal, Neeti Mohan",
+        img: "music-22",
+        src: "music-22"
+    },
+    {
+        name: "Batao Yaad Hain Tumko",
+        artist: "Rahat Fateh Ali Khan",
+        img: "music-23",
+        src: "music-23"
+    },
+    {
+        name: "Pasoori",
+        artist: "Ali Sethi, Shae Gill",
+        img: "music-24",
+        src: "music-24"
+    },
+    {
+        name: "Insane",
+        artist: "AP Dhillon",
+        img: "music-25",
+        src: "music-25"
+    },
+    {
+        name: "Naina Ashq Na Ho",
+        artist: "Arijit Singh",
+        img: "music-26",
+        src: "music-26"
+    },
+    {
+        name: "Khairiyat",
+        artist: "Arijit Singh",
+        img: "music-27",
+        src: "music-27"
+    },
+    {
+        name: "Desh Mere",
+        artist: "Arijit Singh",
+        img: "music-28",
+        src: "music-28"
+    },
+    {
+        name: "Dhokha",
+        artist: "Arijit Singh",
+        img: "music-29",
+        src: "music-29"
+    },
+    {
+        name: "Tera Yaar Hoon Main",
+        artist: "Arijit Sngh, Rochak Kohli",
+        img: "music-30",
+        src: "music-30"
+    },
+    {
+        name: "Chunar",
+        artist: "Arijit Singh",
+        img: "music-31",
+        src: "music-31"
+    },
+    {
+        name: "Galti Se Mistake",
+        artist: "Arijit Singh, Amit Mishra",
+        img: "music-32",
+        src: "music-32"
+    },
+    {
+        name: "Hamari Adhuri Kahani",
+        artist: "Arijit Singh",
+        img: "music-33",
+        src: "music-33"
+    },
+    {
+        name: "Neki Ki Raah",
+        artist: "Mithoon, Arijit Singh",
+        img: "music-34",
+        src: "music-34"
+    },
+    {
+        name: "Humdard",
+        artist: "Mithoon, Arijit Singh",
+        img: "music-35",
+        src: "music-35"
+    },
+    {
+        name: "Mere Yaara",
+        artist: "Arijit Singh, Neeti Mohan",
+        img: "music-36",
+        src: "music-36"
+    },
+    {
+        name: "Nashe Si Chhadh Gayi",
+        artist: "Arijit Singh",
+        img: "music-37",
+        src: "music-37"
+    },
+    {
+        name: "Ae Watan",
+        artist: "Arijit Singh",
+        img: "music-38",
+        src: "music-38"
+    },
+    {
+        name: "Pachtaoge",
+        artist: "Jaani, Arijit Singh",
+        img: "music-39",
+        src: "music-39"
+    }
+    
+];
